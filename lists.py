f0bd8ba9815e24a6916763832d408dd23f3fc89c
danielrv91 <rosalesdaniel91@gmail.com>
2023-09-04 01:11:51 +0000

    Lists

---
+++ lists.py
@@ -0,0 +1,19 @@
+import random
+
+var = []
+
+print(type(var))
+
+var2 = [151, 251, 386, 493, 649, '009']
+
+print(var2)
+
+var2.append(721) #inplace function
+
+print(var2)
+
+var2 = var2 + [1008]
+
+print(var2)
+
+var2.reverse()
